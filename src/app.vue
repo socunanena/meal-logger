863c9cb9e080082cd3a65960847db35b3d0f8dea
Natàlia Calvet i Vidal <natalia.cavi@gmail.com>
2018-07-08 16:49:29 +0200

    Project setup

---
+++ src/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Calendar',
+    data: () => ({}),
+    computed: {},
+  };
+</script>
+
+<style scoped>
+</style>
