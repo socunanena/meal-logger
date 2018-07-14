bddd8950069aefd40ac5951b71a8f368c34cd387
Natàlia Calvet i Vidal <natalia.cavi@gmail.com>
2018-07-14 15:15:18 +0200

    Meal input

--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
+    <form v-on:submit.prevent="addMeal()">
+      <input title="Date" v-model="date" />
+      <input title="Content" v-model="content" />
+      <button type="submit" title="go!" />
+    </form>
   </div>
 </template>
 
 <script>
   export default {
     name: 'Calendar',
-    data: () => ({}),
+    data: () => ({
+      date: null,
+      content: null,
+      meals: [],
+    }),
     computed: {},
+    methods: {
+      addMeal() {
+        this.meals.push({
+          timestamp: new Date(this.date),
+          content: this.content,
+        });
+      },
+    },
   };
 </script>
 
