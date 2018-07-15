400e0112671d717ab75edf26180849a39f251f41
Natàlia Calvet i Vidal <natalia.cavi@gmail.com>
2018-07-15 11:43:53 +0200

    Implemented time picker

--- src/components/meal-input/index.vue
+++ src/components/meal-input/index.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <form v-on:submit.prevent="$emit('add-meal', { date, content })">
-      <input title="Date" v-model="date" />
+      <date-picker
+        type="datetime"
+        format="YYYY-MM-DD HH:mm"
+        :lang="timePickerOptions.lang"
+        first-day-of-week.number=1
+        :time-picker-options="timePickerOptions.timeSteps"
+        v-model="date"
+      />
       <input title="Content" v-model="content" />
       <button type="submit" title="go!" />
     </form>
@@ -9,11 +16,26 @@
 </template>
 
 <script>
+  import DatePicker from 'vue2-datepicker';
+
   export default {
     name: 'MealInput',
+    components: {
+      DatePicker,
+    },
     data: () => ({
-      date: undefined,
+      date: new Date(),
       content: undefined,
+      timePickerOptions: {
+        lang: {
+          days: ['Dl', 'Dt', 'Dc', 'Dj', 'Dv', 'Ds', 'Dg'],
+          months: ['Gener', 'Febrer', 'Març', 'Abril', 'Maig', 'Juny', 'Juliol', 'Agost', 'Setembre', 'Octubre', 'Novembre', 'Desembre'],
+          placeholder: {
+            date: 'Seleccioneu una data',
+          },
+        },
+        timeSteps: { start: '00:00', step: '00:30', end: '23:30' },
+      },
     }),
   };
 </script>
