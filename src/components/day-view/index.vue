1b3bf573c75cd8edbd008f60a2c7ab71f33924cd
Natàlia Calvet i Vidal <natalia.cavi@gmail.com>
2018-07-16 10:12:52 +0200

    Allowed to select the day in day view

--- src/components/day-view/index.vue
+++ src/components/day-view/index.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
+    <div>
+      <date-picker
+              type="date"
+              format="YYYY-MM-DD"
+              :lang="timePickerOptions.lang"
+              first-day-of-week.number=1
+              v-model="day"
+      />
+    </div>
     <ul>
       <li v-for="meal in mealsOfTheDay">
-        <strong>{{ formatDate(meal.date, 'YYYY-MM-DD HH:mm') }}:</strong>
+        <strong>{{ formatDate(meal.date, 'YYYY-MM-DD HH:mm') }} -</strong>
         {{ meal.content }}
       </li>
     </ul>
@@ -11,19 +20,30 @@
 
 <script>
   import fecha from 'fecha';
+  import DatePicker from 'vue2-datepicker';
 
   export default {
     name: 'DayView',
+    components: { DatePicker },
     props: ['meals'],
     data() {
       return {
-        day: new Date().getDate(),
+        day: new Date(),
+        timePickerOptions: {
+          lang: {
+            days: ['Dl', 'Dt', 'Dc', 'Dj', 'Dv', 'Ds', 'Dg'],
+            months: ['Gener', 'Febrer', 'Març', 'Abril', 'Maig', 'Juny', 'Juliol', 'Agost', 'Setembre', 'Octubre', 'Novembre', 'Desembre'],
+            placeholder: {
+              date: 'Seleccioneu una data',
+            },
+          },
+        },
       }
     },
     computed: {
       mealsOfTheDay() {
         return this.meals
-          .filter(meal => meal.date.getDate() === this.day)
+          .filter(meal => fecha.format(meal.date, 'YY-M-D') === fecha.format(this.day, 'YY-M-D'))
           .sort((mealA, mealB) => mealA.date - mealB.date);
       },
     },
