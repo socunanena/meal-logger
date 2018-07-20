e3e230c879651cbc809e630d778fe3dc9f4937e4
Natàlia Calvet i Vidal <natalia.cavi@gmail.com>
2018-07-20 22:27:03 +0200

    Implemented calendar view

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    { src: '~plugins/vue-full-calendar', ssr: false },
     { src: '~plugins/vue2-datepicker', ssr: false },
   ],
 };
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,54 @@
 <template>
-  <ul>
-    <li v-for="meal in mealsOfTheDay">
-      <strong>{{ meal.date }} -</strong>
-      {{ meal.content }}
-    </li>
-  </ul>
+  <no-ssr>
+    <full-calendar :events="mealsOfTheDay" :config="config"/>
+  </no-ssr>
 </template>
 
 <script>
   import fecha from 'fecha';
+  // import 'fullcalendar/dist/locale/ca';
 
   export default {
     head: {
-      title: 'Meals'
+      title: 'Meals',
     },
     asyncData() {
       const mealsOfTheDay = [
-        { date: new Date(2018, 7, 27, 10, 30), content: 'esmorzar' },
-        { date: new Date(2018, 7, 27, 13, 30), content: 'dinar' },
-        { date: new Date(2018, 7, 27, 20, 30), content: 'sopar' },
+        { date: new Date(2018, 6, 20, 10, 30), content: 'esmorzar' },
+        { date: new Date(2018, 6, 20, 13, 30), content: 'dinar' },
+        { date: new Date(2018, 6, 20, 20, 30), content: 'sopar' },
       ];
 
       return {
-        mealsOfTheDay: mealsOfTheDay.map(m => ({ ...m, date: fecha.format(m.date, 'YYYY-MM-DD HH:mm')}))
+        mealsOfTheDay: mealsOfTheDay.map(m => ({
+          start: m.date,
+          title: m.content,
+        })),
+        config: {
+          header: {
+            left: 'today',
+            center: 'prev title next',
+            right: 'agendaWeek,agendaDay',
+          },
+          buttonText: {
+            prev: '<',
+            next: '>',
+          },
+          titleFormat: 'YYYY-MM-DD',
+          allDaySlot: false,
+          minTime: '07:00',
+          maxTime: '22:00',
+          firstDay: 1,
+          columnHeaderFormat: 'dddd',
+          slotLabelFormat: 'HH:mm',
+          themeSystem: 'bootstrap4',
+        },
       };
     },
   };
 </script>
+
+<style>
+  @import '../node_modules/fullcalendar/dist/fullcalendar.css';
+  @import 'https://bootswatch.com/4/cerulean/bootstrap.min.css'; /* https://fullcalendar.io/releases/fullcalendar/3.9.0/demos/themes.html */
+</style>
